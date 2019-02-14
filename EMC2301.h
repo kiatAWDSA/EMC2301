0a567b05116e1c3f360503f37acb9a62739ba16b
kiatAWDSA <lskiat@huskers.unl.edu>
2019-02-14 11:28:10 -0600

    Add project files.

---
+++ EMC2301.h
@@ -0,0 +1,14 @@
+// EMC2301.h
+
+#ifndef _EMC2301_h
+#define _EMC2301_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
---
+++ EMC2301.ino
@@ -0,0 +1,16 @@
+/*
+ Name:		EMC2301.ino
+ Created:	2/14/2019 11:26:51 AM
+ Author:	slau4
+*/
+
+// the setup function runs once when you press reset or power the board
+#include "EMC2301.h"
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
